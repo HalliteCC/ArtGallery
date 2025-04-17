d775d37cc95d18ab79417dafb2485a9dde413048
Caie Hallite <hallitecaie@gmail.com>
2025-04-17 20:52:59 -0300

    Second Commit - App Done

--- app/src/main/java/com/example/artgallery/MainActivity.kt
+++ app/src/main/java/com/example/artgallery/MainActivity.kt
@@ -1,16 +1,43 @@
 package com.example.artgallery
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.artgallery.data.listArtData
 import com.example.artgallery.ui.theme.ArtGalleryTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +47,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArtGalleryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ArtScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +57,107 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ArtScreen(modifier: Modifier = Modifier) {
+    var currentIndex by remember { mutableStateOf(0) }
+    val currentArt = listArtData[currentIndex]
+
+    Scaffold(
+        bottomBar = {
+            Buttons(
+                onPrevious = {
+                    currentIndex = if (currentIndex == 0) listArtData.lastIndex else currentIndex - 1
+                },
+                onNext = {
+                    currentIndex = if (currentIndex == listArtData.lastIndex) 0 else currentIndex + 1
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ArtImage(artImage = currentArt.artImage)
+            Spacer(modifier = Modifier.height(8.dp))
+            ArtInformation(
+                artName = currentArt.artName,
+                authorName = currentArt.authorName,
+                year = currentArt.year
+            )
+        }
+    }
+}
+
+@Composable
+fun ArtImage(@DrawableRes artImage: Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(artImage),
+        contentDescription = "Art Image",
+        modifier.padding(8.dp)
     )
 }
+
+@Composable
+fun ArtInformation(
+    @StringRes artName: Int,
+    @StringRes authorName: Int,
+    year: String
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Text(
+            text = stringResource(artName),
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = stringResource(authorName) + " (1498)",
+        )
+    }
+}
+
+@Composable
+fun Buttons(
+    onPrevious: () -> Unit,
+    onNext: () -> Unit,
+
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Button(
+            onClick = onPrevious,
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.weight(1f)
+        ) {
+            Text("Previous")
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Button(
+            onClick = onNext,
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.weight(1f)
+        ) {
+            Text("Next")
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtGalleryTheme {
-        Greeting("Android")
+        ArtScreen()
     }
 }
--- app/src/main/java/com/example/artgallery/data/ArtGalleryData.kt
+++ app/src/main/java/com/example/artgallery/data/ArtGalleryData.kt
@@ -6,14 +6,15 @@ import com.example.artgallery.R
 
 data class ArtGalleryData(
     @DrawableRes val artImage: Int,
+    @StringRes val artName: Int,
     @StringRes val authorName: Int,
-    val year: Int
+    val year: String
 )
 
 val listArtData = listOf(
-    ArtGalleryData(R.drawable.mona_lisa, R.string.leonardo_da_vinci, 1504),
-    ArtGalleryData(R.drawable.guernica, R.string.pablo_picasso, 1937),
-    ArtGalleryData(R.drawable.criacao_adao, R.string.michelangelo_buonarroti, 1511),
-    ArtGalleryData(R.drawable.a_ultima_ceia, R.string.leonardo_da_vinci, 1498),
-    ArtGalleryData(R.drawable.a_noite_estrelada, R.string.vincent_van_gogh, 1889),
+    ArtGalleryData(R.drawable.mona_lisa, R.string.mona_lisa, R.string.leonardo_da_vinci, "1504"),
+    ArtGalleryData(R.drawable.guernica, R.string.guernica,R.string.pablo_picasso, "1937"),
+    ArtGalleryData(R.drawable.criacao_adao, R.string.criacao_adao,R.string.michelangelo_buonarroti, "1511"),
+    ArtGalleryData(R.drawable.a_ultima_ceia,R.string.a_ultima_ceia, R.string.leonardo_da_vinci, "1498"),
+    ArtGalleryData(R.drawable.a_noite_estrelada, R.string.a_noite_estrelada,R.string.vincent_van_gogh, "1889"),
 )
