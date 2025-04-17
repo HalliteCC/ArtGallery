7c775bbb6382f646caf2e7358e672345588f6687
Caie Hallite <hallitecaie@gmail.com>
2025-04-17 19:48:34 -0300

    First Commit - Adding Images and Creating data class

---
+++ app/src/main/java/com/example/artgallery/data/ArtGalleryData.kt
@@ -0,0 +1,19 @@
+package com.example.artgallery.data
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.example.artgallery.R
+
+data class ArtGalleryData(
+    @DrawableRes val artImage: Int,
+    @StringRes val authorName: Int,
+    val year: Int
+)
+
+val listArtData = listOf(
+    ArtGalleryData(R.drawable.mona_lisa, R.string.leonardo_da_vinci, 1504),
+    ArtGalleryData(R.drawable.guernica, R.string.pablo_picasso, 1937),
+    ArtGalleryData(R.drawable.criacao_adao, R.string.michelangelo_buonarroti, 1511),
+    ArtGalleryData(R.drawable.a_ultima_ceia, R.string.leonardo_da_vinci, 1498),
+    ArtGalleryData(R.drawable.a_noite_estrelada, R.string.vincent_van_gogh, 1889),
+)
